97419734a410363e0819bb9ccc7c814011c02f15
Luka Mutić <lukamuticxy@gmail.com>
2025-06-02 22:13:43 +0200

    luka: added About Intro Block v1

---
+++ wp-content/themes/tld-cargo/blocks/about-intro-block-v1/about-intro-block-v1.php
@@ -0,0 +1,55 @@
+<?php
+
+?>
+
+        <section class="bg-light-gray wide-tb-100">
+            <div class="container pos-rel">
+                <div class="row">
+                    <div class="col-lg-6 ms-auto mb-0">
+                        <!-- Heading Main -->
+                        <div class=" wow fadeInDown" data-wow-duration="0" data-wow-delay="0s">
+                            <h1 class="heading-main text-start mb-5">
+                                <span>WHO WE ARE</span>About Us</h1>
+                        </div>
+                        <!-- Heading Main -->
+
+                        <p class="lead fw-5 txt-blue">Whether you require distribution or fulfillment, defined freight
+                            forwarding, or a complete supply chain solution, we are here for you.</p>
+
+                        <div class="mt-5">
+                            <ul class="nav nav-pills theme-tabbing mb-3" id="pills-tab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" href="#pills-home"
+                                        role="tab" aria-controls="pills-home" aria-selected="true">About us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" href="#pills-profile"
+                                        role="tab" aria-controls="pills-profile" aria-selected="false">Our History</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content theme-tabbing" id="pills-tabContent">
+                                <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                                    aria-labelledby="pills-home-tab">
+                                    <p>Collaboratively administrate empowered markets via plug-and-play networks.
+Dynamically procrastinate B2C users after installed base benefits. </p>
+                                    <p>We don’t just manage suppliers, we micro-manage them. We have a consultative,
+                                        personalized approach. Dramatically visualize customer directed convergence
+                                        without revolutionary ROI.</p>
+                                </div>
+                                <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                                    aria-labelledby="pills-profile-tab">
+                                    <p>Collaboratively administrate empowered markets via plug-and-play networks.
+Dynamically procrastinate B2C users after installed base benefits. </p>
+                                    <p>Dramatically visualize customer directed convergence without revolutionary ROI.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="img-business-man">
+                        <img src="assets/images/courier-man.png" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </section>
